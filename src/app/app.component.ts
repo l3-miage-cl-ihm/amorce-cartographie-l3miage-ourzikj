d1d52e7d86d44044c43373c7e415a37349c9f595
Alexandre Demeure <alexandre.demeure@univ-grenoble-alpes.fr>
2024-03-28 10:43:15 +0100

    trop de calcul de markers !!! à chaque fois qu'on en ajoute un, ça recalcule tout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, Signal, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import { MapOptions, tileLayer, LatLng, LatLngLiteral, marker, icon, Icon, LeafletMouseEvent } from 'leaflet';
+import { MapOptions, tileLayer, LatLng, LatLngLiteral, marker, icon, Icon, LeafletMouseEvent, Layer } from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -11,34 +11,39 @@ import { MapOptions, tileLayer, LatLng, LatLngLiteral, marker, icon, Icon, Leafl
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  readonly defaultIcon = computed( () => icon({
+  readonly defaultIcon = computed(() => icon({
     ...Icon.Default.prototype.options,
     iconUrl: 'assets/marker-icon.png',
     iconRetinaUrl: 'assets/marker-icon-2x.png',
     shadowUrl: 'assets/marker-shadow.png'
-  }) );
-  
-  readonly center = signal<LatLng>( new LatLng(45.166672, 5.71667) );
+  }));
+
+  readonly center = signal<LatLng>(new LatLng(45.166672, 5.71667));
   readonly zoom = signal<number>(12);
 
   readonly places = signal<readonly LatLngLiteral[]>([
     { lat: 48.858370, lng: 2.294481 },                   // Tour Eiffel
     { lat: 45.193866812447716, lng: 5.768449902534485 }, // UFR IM2AG
-    { lat: 44.56414746666089, lng: 6.495827436447144},   // Place Bartelon
+    { lat: 44.56414746666089, lng: 6.495827436447144 },   // Place Bartelon
   ]);
-  
-  readonly options: Signal<MapOptions> = computed(() => ({
-    layers: [
-      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
-      ...this.places().map( ({lat, lng}) => marker([lat, lng], {icon: this.defaultIcon()}).bindPopup(`<p>${ [lat, lng]}</p>`) )
-    ]
-  }) )
 
-  
-  addMarker(event:LeafletMouseEvent){
-      console.log(event);
-      this.places.update(value =>[...value,event.latlng] )
-      console.log(this.places()) 
+  private readonly sigMarkers = computed(() => {
+    return this.places().map(({ lat, lng }) => {
+      console.log(lat, lng);
+      return marker([lat, lng], { icon: this.defaultIcon() }).bindPopup(`<p>${[lat, lng]}</p>`)
+    })
+  });
+
+  readonly layers: Signal<Layer[]> = computed(() => [
+    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
+    ...this.sigMarkers()
+  ])
+
+
+  addMarker(event: LeafletMouseEvent) {
+    console.log(event);
+    this.places.update(value => [...value, event.latlng])
+    console.log(this.places())
 
   }
 
